d778de59da1db146f234e503c4f8a00b9c7a1256
YassinSouidi <47624749+YassinSouidi@users.noreply.github.com>
2020-10-15 19:50:26 +0200

    Complete Exo2

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,6 +10,18 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (wr spaceEraser) Read(buffer []byte) (int, error) {
+	n, err := wr.r.Read(buffer)
+	noSpaces := 0
+	for i := 0; i < n; i++ {
+		if buffer[i] != 32 {
+			buffer[noSpaces] = buffer[i]
+			noSpaces++
+		}
+	}
+	return noSpaces, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
